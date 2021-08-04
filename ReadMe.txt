8afffab50dfb8ac3cfd8aed1f314660d1a4b4bf7
Syuichi Murashima <mura-github@vwnet.jp>
2021-08-04 14:47:27 +0900

    1st commit

---
+++ ReadMe.txt
@@ -0,0 +1,26 @@
+﻿grep by PowerShell
+
+■ これは何?
+いわゆる grep の PowerShell 実装
+
+■ 使い方
+grep 正規表現 ファイルパターン -Recurse
+
+■ 例
+grep 18[0-9] c:\temp\*.txt -Recurse
+
+■ Install 方法
+install.ps1 を実行してください
+
+■ Uninstall 方法
+uninstall.ps1 を実行して下さい
+(問い合わせが来たら Enter)
+
+■ 動作確認環境
+Windows PowerShell 5.1
+PowerShell 7.1.3(Windows)
+
+■ リポジトリ
+https://github.com/MuraAtVwnet/grep
+git@github.com:MuraAtVwnet/grep.git
+
---
+++ grep.psm1
@@ -0,0 +1,16 @@
+﻿#########################################
+# grep by PowerShell
+#########################################
+function grep( $MatchPattern, $FilePattern, [switch]$Recurse ){
+	if($Recurse){
+		[array]$Files = dir $FilePattern -Recurse
+	}
+	else{
+		[array]$Files = dir $FilePattern
+	}
+
+	foreach($File in $Files){
+		$FileName = $File.FullName
+		Select-String -Path $FileName -Pattern $MatchPattern -CaseSensitive
+	}
+}
