38ef9e30efbfd6271150f346ebf1ad37f9324413
VWNET\mura <mura@vwnet.jp>
2024-10-25 17:34:14 +0900

    Online install 対応

--- ReadMe.txt
+++ ReadMe.txt
@@ -10,17 +10,27 @@ grep 正規表現 ファイルパターン -Recurse
 grep 18[0-9] c:\temp\*.txt -Recurse
 
 ■ Install 方法
-install.ps1 を実行してください
+# 以下を PowerShell プロンプトにコピペ
+
+$ModuleName = "grep"
+$GitHubName = "MuraAtVwnet"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ModuleName/master/OnlineInstall.ps1 -OutFile ~/OnlineInstall.ps1
+& ~/OnlineInstall.ps1
 
 ■ Uninstall 方法
-uninstall.ps1 を実行して下さい
+~/UnInstallgrep.ps1 を実行して下さい
 (問い合わせが来たら Enter)
 
 ■ 動作確認環境
 Windows PowerShell 5.1
-PowerShell 7.3.9(Windows)
+PowerShell 7.4.5(Windows)
 
 ■ リポジトリ
 https://github.com/MuraAtVwnet/grep
 git@github.com:MuraAtVwnet/grep.git
+
+
+■ Web site
+PowerShell 版 grep
+https://www.vwnet.jp/windows/PowerShell/2021080401/GrepByPowerShell.htm
 
--- grep.psm1
+++ grep.psm1
@@ -1,7 +1,53 @@
 ﻿#########################################
 # grep by PowerShell
 #########################################
-function grep( [array]$MatchPattern, [array]$FilePattern, [switch]$Recurse ){
+function grep( [array]$MatchPattern, [array]$FilePattern, [switch]$Recurse, [switch]$VertionCheck ){
+
+	if( $VertionCheck ){
+		$ModuleName = "grep"
+		$GitHubName = "MuraAtVwnet"
+		$Module = $ModuleName + ".psm1"
+		$Installer = "Install" + $ModuleName + ".ps1"
+		$UnInstaller = "UnInstall" + $ModuleName + ".ps1"
+		$Vertion = "Vertion" + $ModuleName + ".txt"
+		$VertionTemp = "VertionTemp" + $ModuleName + ".tmp"
+
+		$Update = $False
+
+		if( -not (Test-Path ~/$Vertion)){
+			$Update = $True
+		}
+		else{
+			$LocalVertion = Get-Content -Path ~/$Vertion
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ModuleName/master/Vertion.txt -OutFile ~/$VertionTemp
+			$NowVertion = Get-Content -Path ~/$VertionTemp
+			Remove-Item ~/$VertionTemp
+
+			if( $LocalVertion -ne $NowVertion ){
+				$Update = $True
+			}
+		}
+
+		if( $Update ){
+			Write-Output "最新版に更新します"
+			Write-Output "更新完了後、PowerShell プロンプトを開きなおしてください"
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ModuleName/master/$Module -OutFile ~/$Module
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ModuleName/master/install.ps1 -OutFile ~/$Installer
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ModuleName/master/uninstall.ps1 -OutFile ~/$UnInstaller
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ModuleName/master/Vertion.txt -OutFile ~/$Vertion
+			& ~/$Installer
+			Remove-Item ~/$Module
+			Remove-Item ~/$Installer
+			Write-Output "更新完了"
+			Write-Output "PowerShell プロンプトを開きなおしてください"
+		}
+		else{
+			Write-Output "更新の必要はありません"
+		}
+		return
+	}
+
+
 	if($Recurse){
 		[array]$Files = Get-ChildItem $FilePattern -Recurse
 	}
